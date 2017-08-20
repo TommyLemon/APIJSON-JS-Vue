ad4b1b620ff33ae9ffbd5e258deda6a25f122c43
TommyLemon <1184482681@qq.com>
2017-08-20 20:54:57 +0800

    新增查询数组及双向绑定

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,39 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <div class="#show">动态内容: {{ Moment.content }}</div>
-    <div class="#show">用户名: {{ User.name }}</div>
-    <button @click="ok()" >{{ msg }}</button>
+    <div class="#show">动态内容: {{ this.Moment.content}}</div>
+    <div class="#show">发布者: {{ this.User.name }}</div>
+    <button @click="ok()" >{{ this.msg }}</button>
+    <ul>
+      <div v-for="item in commentList">
+        {{ item.User.name + ': ' + item.Comment.content }}
+      </div>
+    </ul>
   </div>
 </template>
 <script>
   import axios from 'axios'
-
   // axios.defaults.withCredentials = true
 
+  let url_base = "http://139.196.140.118:8080"
+  let url_get = url_base + "/get"
+  let url_head = url_base + "/head"
+  let url_post_get = url_base + "/post_get"
+  let url_post_head = url_base + "/post_head"
+  let url_post = url_base + "/post"
+  let url_put = url_base + "/put"
+  let url_delete = url_base + "/delete"
 
-  var url_base = "http://139.196.140.118:8080"
-  var url_get = url_base + "/get"
-  var url_head = url_base + "/head"
-  var url_post_get = url_base + "/post_get"
-  var url_post_head = url_base + "/post_head"
-  var url_post = url_base + "/post"
-  var url_put = url_base + "/put"
-  var url_delete = url_base + "/delete"
-
-  var model = {
+  let model = {
     msg: 'APIJSON',
     Moment: {
-      content:'content'
+      content: 'content'
     },
     User: {
-      name:'name'
+      name: 'name'
+    },
+    commentList: {
+
     }
   }
 
@@ -40,34 +46,57 @@
       ok() {
         // 刷新界面
         function refresh(data) {
-          if (data == null) {
-            data = {}
-          }
-
-          model.Moment = data.Moment == null ? {} : data.Moment
-          model.User = data.User == null ? {} : data.User
+          data = data || {}
+          model.Moment = data.Moment || {}
+          model.User = data.User || {}
+          model.commentList = data['[]'] || []
 
           console.log('refresh  model = \n' + JSON.stringify(model))
         }
 
-        axios.get(url_get + '/' + JSON.stringify(
-        {
-          Moment: {
-            userId: 70793
-          },
-          User: {
-            id: 70793
-          }
-        }),
-        {
-          params: {}
-        })
-        .then(function (response) {
-          refresh(response.data)
-        })
-        .catch(function (error) {
-          console.log(error);
-        })
+        axios.get(url_get + '/' + encodeURIComponent(JSON.stringify(
+            {
+              'Moment': {
+                'userId': 93793
+              },
+              'User': {
+                'id': 93793
+              },
+              '[]': {
+                'count': 5,
+                'Comment': {
+                  'momentId@': encodeURIComponent('Moment/id')
+                },
+                'User': {
+                  'id@': encodeURIComponent('/Comment/userId')
+                }
+              }
+            })), { params: {} })
+          .then(function (response) {
+            refresh(response.data)
+          })
+          .catch(function (error) {
+            console.log(error);
+          })
+
+
+//        request(url_get, {
+//          'Moment': {
+//            'userId': 93793
+//          },
+//          'User': {
+//            'id': 93793
+//          },
+//          '[]': {
+//            'count': 5,
+//            'Comment': {
+//              'momentId@': encodeURIComponent('Moment/id')
+//            },
+//            'User': {
+//              'id@': encodeURIComponent('/Comment/userId')
+//            }
+//          }
+//        })
 
       }
     }
